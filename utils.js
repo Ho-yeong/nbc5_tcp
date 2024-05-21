a9ac0b33ed27037aea751c982f294bf0b0bce3b8
simon <ghdud0503@gmail.com>
2024-05-21 17:05:06 +0900

    separate util file

--- utils.js
+++ utils.js
@@ -10,8 +10,8 @@ export const readHeader = (buffer) => {
 
 // 메시지 길이와 핸들러 ID를 버퍼로 변환 (Big Endian)
 export const writeHeader = (length, handlerId) => {
-  const buffer = Buffer.alloc(HEADER_SIZE);
-  buffer.writeUInt32BE(length, LENGTH_OFFSET);
-  buffer.writeUInt16BE(handlerId, HANDLER_ID_OFFSET);
+  const buffer = Buffer.alloc(HEADER_SIZE); // HEADER_SIZE 는 6바이트
+  buffer.writeUInt32BE(length, LENGTH_OFFSET); // 메시지 길이를 빅엔디안 방식으로 기록 (4바이트)
+  buffer.writeUInt16BE(handlerId, HANDLER_ID_OFFSET); // 핸들러 ID를 빅엔디안 방식으로 기록 (2바이트)
   return buffer;
 };
